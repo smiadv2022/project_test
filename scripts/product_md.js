7203dbca772aba540906746cb85ec03b531c8531
smiadv2022 <msorokina@spacemedia.com.ua>
2023-01-08 01:00:31 +0200

    hero1

--- scripts/product_md.js
+++ scripts/product_md.js
@@ -1,15 +1,21 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-mobmenu-open]"),
-      closeModalBtn: document.querySelector("[data-mobmenu-close]"),
-      modal: document.querySelector("[data-mobmenu]"),
+      openModalBtn: document.querySelector("[data-product-open1]"),
+      openModa2Btn: document.querySelector("[data-product-open2]"),
+      openModa3Btn: document.querySelector("[data-product-open3]"),
+      closeModalBtn: document.querySelector("[data-product-close]"),
+     
+      modal: document.querySelector("[data-product]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModa2Btn.addEventListener("click", toggleModal);
+    refs.openModa3Btn.addEventListener("click", toggleModal);
+  
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
-      refs.modal.classList.toggle("is-menu");
+      refs.modal.classList.toggle("is-hidden-product");
     }
   })();
   
